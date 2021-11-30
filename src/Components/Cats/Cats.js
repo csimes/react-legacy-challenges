9876a1d00edea2f9b6f741ae0a0f71bbbfb70997
Cherron Simes <cherron@developdbycherron.com>
2021-11-29 21:32:01 -0600

    Added clarification for completed objectives with styling

--- src/Components/Cats/Cats.js
+++ src/Components/Cats/Cats.js
@@ -4,23 +4,48 @@ import { Container } from 'reactstrap';
 
 const Cats = () => {
   return (
-    <Container className="App">
-      <br/>
-      <h1 className="App-intro">
-        To complete this challenge, complete and fix the existing code to have it do the following:
-      </h1>
+      <Container className="App">
+          <br />
+          <p className="App-intro">
+              To complete this challenge, complete and fix the existing code to
+              have it do the following:
+          </p>
 
-      <hr />
-        
-      <ul>
-        <li>Render the list of cat breeds to the screen</li>
-        <li>Use props correctly</li>
-        <li>Use .map() correctly</li>
-        <li>Ensure there are no errors</li>
-      </ul>
-      <hr />
-      <CatIndex />
-    </Container>
+          <hr />
+
+          <ul>
+              <li>
+                  Render the list of cat breeds to the screen <strong>|</strong>{" "}
+                  <em>Objective completed</em>!{" "}
+                  <span role="img" aria-label="checkmark">
+                      ✅
+                  </span>
+              </li>
+              <li>
+                  Use props correctly <strong>|</strong>{" "}
+                  <em>Objective completed</em>!{" "}
+                  <span role="img" aria-label="checkmark">
+                      ✅
+                  </span>
+              </li>
+              <li>
+                  Use .map() correctly <strong>|</strong>{" "}
+                  <em>Objective completed</em>!{" "}
+                  <span role="img" aria-label="checkmark">
+                      ✅
+                  </span>
+              </li>
+              <li>
+                  Ensure there are no errors <strong>|</strong>{" "}
+                  <em>Objective completed</em>!{" "}
+                  <span role="img" aria-label="checkmark">
+                      ✅
+                  </span>
+              </li>
+          </ul>
+          <hr />
+          <CatIndex />
+      </Container>
   );
 
 }
