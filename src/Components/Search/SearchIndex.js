703c5387be0440b4b756bd6e6bed8d7f11091aaa
csimes <cherron@developdbycherron.com>
2022-02-01 19:39:07 -0600

    Search function refactored and completed

--- src/Components/Search/SearchIndex.js
+++ src/Components/Search/SearchIndex.js
@@ -2,50 +2,67 @@ import React, { Component } from 'react';
 import { Input, Form } from 'reactstrap';
 
 class SearchIndex extends Component {
-  
-  constructor(){
-    super()
-      this.state = {
-        things: ['pen', 'marker', 'eraser', 'notebook', 'pencil', 'scissors', 'highlighter', 'stapler', 'paper clip', 'binder', 'hole punch', 'laminator', 'laminating sheets', 'protective sheets', 'index cards'] ,
-        searchTerm: "",
-        results: ""
-      }
-  }
-inputHandler(e){
-    this.setState({
-      searchTerm: e.target.value
-    })
-  }
-  searchFunction = (e) => {
-    e.preventDefault();
-    this.setState({ 
-      things : [...this.state.things],
-      results : this.state.things.filter((item) => item === this.state.searchTerm) 
-    })
+  constructor() {
+    super();
+    this.state = {
+      things: [
+        'pen',
+        'marker',
+        'eraser',
+        'notebook',
+        'pencil',
+        'scissors',
+        'highlighter',
+        'stapler',
+        'paper clip',
+        'binder',
+        'hole punch',
+        'laminator',
+        'laminating sheets',
+        'protective sheets',
+        'index cards',
+      ],
+      searchTerm: '',
+      results: '',
+    };
   }
 
-  
+  inputHandler(e) {
+    this.setState({
+      searchTerm: e.target.value,
+    });
+  }
+
+  searchFunction = (e) => {
+    e.preventDefault();
+    this.setState({
+      results: this.state.things.map((things) => {
+        if (things.includes(this.state.searchTerm)) {
+          return <li>{things}</li>;
+        }
+      }),
+    });
+  };
+
   render() {
     return (
-        <div>
-            <Form onSubmit={this.searchFunction}>
-                <Input
-                    type="text"
-                    id="search"
-                    placeholder="Search + Enter"
-                    onChange={(e) => this.inputHandler(e)}
-                    value={this.state.searchTerm}
-                />
-            </Form>
-            {this.state.searchTerm !== "" && this.state.results !== "" ? 
-                <h3> Results: {this.state.results}</h3>
-            : 
-                this.state.things.map((thing) => <li>{thing}</li>
-            )}
-        </div>
+      <div>
+        {/* Changed to onKeyUp so search term wouldn't stay "stuck" when next search was performed */}
+        <Form onKeyUp={this.searchFunction}>
+          <Input
+            type='text'
+            id='search'
+            placeholder='Search'
+            onChange={(e) => this.inputHandler(e)}
+            value={this.state.searchTerm}
+          />
+        </Form>
+        {this.state.searchTerm !== '' && this.state.results !== ''
+          ? this.state.results
+          : this.state.things.map((thing) => <li>{thing}</li>)}
+      </div>
     );
   }
 }
 
 export default SearchIndex;
-
