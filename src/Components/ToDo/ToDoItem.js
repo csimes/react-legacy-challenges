fd98d13b52a82b2977e533e13a07337b43a9294f
Cherron Simes <cherron@developdbycherron.com>
2022-05-03 20:33:50 -0500

    Updated styling

--- src/Components/ToDo/ToDoItem.js
+++ src/Components/ToDo/ToDoItem.js
@@ -5,8 +5,9 @@ export default class ToDoItem extends Component {
   render() {
     return (
       <ListGroup>
-        <ListGroupItem>{this.props.toDoItem.task}</ListGroupItem>
         <ListGroupItem>
+          {this.props.toDoItem.task}
+          <br />
           <Button color='warning'>Edit</Button>
           <Button color='danger'>Delete</Button>
         </ListGroupItem>
